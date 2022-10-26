c588fd6ff1836d06ee421625d1f9823195365d24
wvveluw <wv.veluw@yahoo.co.uk>
2022-10-26 11:26:37 +0200

    Set up a small .R file

---
+++ Assignment2/Assignment2.R
@@ -0,0 +1,12 @@
+### Load packages
+library(tidyverse)
+library(mlbench)      # The dataset is in this package
+data(Glass)           # Let R know that we want to use the dataset "Glass"
+
+### Data wrangling
+
+### Exploratory Analysis
+
+### Simple Models
+
+### More Complex Model
